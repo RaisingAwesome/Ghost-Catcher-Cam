a788f154c3df2934386e36b506553d278c35f423
Sean Miller <38836869+RaisingAwesome@users.noreply.github.com>
2020-08-21 21:07:19 -0500

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-ssid="hello"
-pwd="goodbye"
-os.system("echo \"ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\\nupdate_config=1\\ncountry=US\\n\\nnetwork={\\nssid=\\\"" + ssid + "\\\"\\npsk=\\\"" + pwd + "\\\"\\nkey_mgmt=WPA-PSK\\n}\\n\" > /home/pi/Ghost-Catcher-Cam/tempwifi.cfg")
-os.system("more tempwifi.cfg")
-exit()
-
